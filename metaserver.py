0fabb4c167ce9ef3250f3f309847244118af2e9d
programical <32033530+programical@users.noreply.github.com>
2020-04-28 14:35:32 +0200

    hotfixfix

--- metaserver.py
+++ metaserver.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-import os
+import os, time
 
 while True:
+    os.system("./server.py pre")
     os.system("git pull origin master")
     os.system("./server.py")
+    time.sleep(22)

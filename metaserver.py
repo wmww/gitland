8f2267bc006d25cd0a52bcfa1aaf7cd76f046895
Richie Bendall <richiebendall@gmail.com>
2020-05-03 02:32:17 +1200

    Run the function on time

--- metaserver.py
+++ metaserver.py
@@ -3,7 +3,11 @@
 import os, time
 
 while True:
+    start_time = time.time()
+    
     os.system("./server.py pre")
     os.system("git pull origin master")
     os.system("./server.py post")
-    time.sleep(50)
+    
+    elapsed_time = time.time() - start_time
+    time.sleep(60 - elapsed_time)
